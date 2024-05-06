57aee9cb4349e9689098096911d726db3a2ff13e
KallosP <97070073+KallosP@users.noreply.github.com>
2024-05-06 12:00:34 -0700

    update

--- src/api/task_crud.py
+++ src/api/task_crud.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from src.api import auth
 from src import database as db
 import sqlalchemy
+from pydantic import BaseModel
+from datetime import datetime
 
 router = APIRouter(
     prefix="/crud",
@@ -9,9 +11,20 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.post("/create")
-def create_task():
-    # TODO: Insert a task 
-    #with db.engine.begin() as connection:
+class Task(BaseModel):
+    task_id: int
+    name: str
+    description: str
+    priority: str
+    status: str
+    start_date: datetime.isoformat
+    due_date: datetime.isoformat
+    end_date: datetime.isoformat
 
+@router.post("/create")
+def create_task(task: Task):
+    # TODO: Insert a task 
+    # TODO: generate unique id 
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text(""))
     return "OK"
